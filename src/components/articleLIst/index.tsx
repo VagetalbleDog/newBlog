6e78e5a58494d533184519d5fd898eb8deca2885
zhuwenfu <2583243657@qq.com>
2022-06-12 22:47:27 +0800

    feat:文章列表功能完善

--- src/components/articleLIst/index.tsx
+++ src/components/articleLIst/index.tsx
@@ -1,10 +1,19 @@
 import styled from '@emotion/styled'
-import { Divider, List, message, Skeleton } from 'antd'
+import { Avatar, Button, Divider, List, message, Skeleton, Tag } from 'antd'
+import {ReadOutlined} from '@ant-design/icons';
 import { AxiosResponse } from 'axios'
 import React, { useEffect, useState } from 'react'
 import Service from '../../service'
 import InfiniteScroll from 'react-infinite-scroll-component'
-import { Article, Avatar, Author, Category } from '../../interface/articleList'
+import { Article, Author, Category } from '../../interface/articleList'
+import { Map } from 'typescript';
+
+
+const tagColors:string[] = ["cyan","gold","blue","purple"]
+
+const descriptions = new Map();
+
+
 const ArticleList: React.FC = () => {
   const [artList, setArtList] = useState<Article[]>([])
 
@@ -19,12 +28,21 @@ const ArticleList: React.FC = () => {
     if (nextPage === 10086) {
       message.info('已经全部加载完毕啦！您也可以发表文章哦！')
     }
-    console.log(1)
     setLoading(true)
     Service.get('/api/article/', { page: nextPage })
       .then((res: any) => {
         setArtList([...artList, ...res.data.results])
-        message.success('加载成功')
+        let requestArr = []
+        for(let item of res.data.results){
+          requestArr.push(Service.get(`/api/article/${item.id}`).then((res:any)=>{
+            descriptions.set(item.id,res.data.body.slice(0,50))
+          }))
+        }
+        Promise.all(requestArr).then(()=>{
+          console.log('加载成功')
+        }).catch((err)=>{
+          message.error(err)
+        })
         if (res.data.next === null) {
           setNextPage(10086)
         } else {
@@ -56,77 +74,86 @@ const ArticleList: React.FC = () => {
           size="large"
           renderItem={(item: any) => {
             return (
-              <List.Item className="listItem">
+              <List.Item className="listItem" key={item.id}>
                 <div>
-                  <span className="category">{item.category.title}</span>
-                  {item.tags.map((tag: string) => {
-                    return <span className="tag">{tag}</span>
+                  <span className="category"><Button type='primary' shape='round'>{item.category.title}</Button></span>
+                  {item.tags.map((tag: string,index:number) => {
+                    return <span className='tag' key={index}><Tag color={tagColors[index%4]}>{tag}</Tag></span>
                   })}
                 </div>
                 <a className="title" href={item.url}>
                   {item.title}
+                  <div className='readIcon'><ReadOutlined style={{fontSize:50}} /></div>
+                  <div className='draft'>
+                    <List.Item.Meta avatar={<Avatar size={40} src={'https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png'} />} description={descriptions.get(item.id)?descriptions.get(item.id):'相关内容正在加载中....'}/>
+                  </div>
                 </a>
                 <div className="otherDetail">
-                  <span className="created">发布时间：{new Date(item.created).toDateString()}</span>
-                  <span className="updated">
-                    最后修改于：{new Date(item.updated).toDateString()}
+                  <span className="created">
+                    发布时间：{new Date(item.created).getFullYear()}/{new Date(item.created).getMonth()+1}/{new Date(item.created).getDate()}
                   </span>
-                  <span className="author">Created By：{item.author.username}</span>
+                  <span className="updated">
+                    最后修改于：{new Date(item.updated).getFullYear()}/{new Date(item.updated).getMonth()+1}/{new Date(item.updated).getDate()}
+                  </span>
+                  <span className="author">Created By:{item.author.username}</span>
                 </div>
               </List.Item>
             )
           }}
-        ></List>
+        >
+        </List>
       </InfiniteScroll>
     </Container>
   )
 }
 const Container = styled.div`
-  width: 60%;
+width: 60%;
   .listItem {
+    transform:translate(10%);
     width: 80%;
     margin-top: 20px;
     transition: all 0.5s;
   }
   .listItem:hover {
-    transform: translate(40px);
+    transform: translate(10%,-20px);
     box-shadow: rgba(0, 0, 0, 0.2) 10px 10px 10px 10px;
   }
   .title {
+    margin-top:40px;
+    margin-left: 20px;
+    margin-bottom:30px;
     display: block;
     font-size: 23px;
-    margin: 40px;
   }
-  .tag {
-    display: inline-block;
-    background-color: aqua;
-    border-radius: 15px;
+  .tag{
     margin-left: 10px;
-    padding: 0 10px;
-    font-size: 15px;
-    color: goldenrod;
+  }
+  .draft{
+    margin-left: 10px;
+    margin-top: 20px;
+    font-size: 14px;
+    color:gray
+  }
+  .readIcon{
+    float: right;
   }
   .category {
-    border: 1px solid gold;
-    background-color: gold;
     display: inline-block;
-    border-radius: 20px;
-    padding: 0 5px;
     font-size: 18px;
     color: whitesmoke;
   }
   .created {
-    font-size: 16px;
+    font-size: 10px;
     color: violet;
     margin: 0 10px;
   }
   .updated {
-    font-size: 16px;
+    font-size: 10px;
     color: green;
     margin: 0 10px;
   }
   .author {
-    font-size: 20px;
+    font-size: 14px;
   }
 `
 export default ArticleList
